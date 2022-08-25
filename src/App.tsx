aba7448f77130490c5e80d240a2ef1bf298b8e5d
caarlosdamian <dapecarlos@gmail.com>
2022-08-24 21:01:51 -0500

    feat: added grid and grid items WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import './App.scss'
+import "./App.scss";
+import { gridItems } from "./utils";
 
 function App() {
-
   return (
-    <div className="App">
-
-    </div>
-  )
+    <main className="App">
+      <div className="grid__container">
+        {gridItems.map((item) => (
+          <div className={`grid__item ${item.color} ${item.id}`} key={item.id}>
+            <div className="grid__top--container">
+              <div className="grid__top-left">
+                <img src="" alt="" className="grid_img-profile" />
+              </div>
+              <div className=" grid__top-right">
+                <h1 className="grid__top-title">{item.name}</h1>
+                <span className="grid__top-subtitle">{item.position}</span>
+              </div>
+            </div>
+            <div className="grid__bottom--container"></div>
+            {item.name}
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
