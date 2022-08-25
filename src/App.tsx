cc5f23b4931ef68d34132e7be83b39993b931cd9
caarlosdamian <dapecarlos@gmail.com>
2022-08-25 09:11:47 -0500

    feat: added grid desktop styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,26 @@ function App() {
       <div className="grid__container">
         {gridItems.map((item) => (
           <div className={`grid__item ${item.color} ${item.id}`} key={item.id}>
+           {item.img && <img src={item.img} alt={item.img}  className='absolut-img'/>} 
             <div className="grid__top--container">
               <div className="grid__top-left">
-                <img src="" alt="" className="grid_img-profile" />
+                <img
+                  src={item.profilePhoto}
+                  alt={item.profilePhoto}
+                  className="grid_img-profile"
+                />
               </div>
               <div className=" grid__top-right">
-                <h1 className="grid__top-title">{item.name}</h1>
-                <span className="grid__top-subtitle">{item.position}</span>
+                <h1 className={`grid__top-title ${item.color}`}>{item.name}</h1>
+                <span className={`grid__top-subtitle ${item.color}`}>
+                  {item.position}
+                </span>
               </div>
             </div>
-            <div className="grid__bottom--container"></div>
-            {item.name}
+            <div className="grid__bottom--container">
+              <p className={`grid__bottom--description ${item.color}`}>{item.text}</p>
+              <p className={`grid__bottom--description-second ${item.color}`}>{item.subText}</p>
+            </div>
           </div>
         ))}
       </div>
